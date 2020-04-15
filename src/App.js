d27836f8593673089fb30d4e737f7dc96af53c31
babybreath101 <annastasyafar04@gmail.com>
2020-04-15 16:26:26 +0700

    try to deploy

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
-import logo from './logo.svg';
+import './App.css';
 import {
   BrowserRouter,
   Switch,
-} from "react-router-dom";
+  Route,
+} 
+from "react-router-dom";
 import Header from "./Header";
 import Blog from "./Blog";
+import BlogDetail from "./BlogDetail";
 
 //install bootstrap dulu
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -45,16 +21,10 @@ function App() {
       <div className="App">
         <Header />
         <Switch>
-          <Route path="/about">
-            About
-          </Route>
-          <Route path="/blog/:articleId" component=BlogDetail />
-          <Route path="/blog">
-            <Blog />
-          </Route>
-          <Route path="/">
-            Home
-          </Route>
+          <Route path="/about">About</Route>
+          <Route path="/blog/:articleId" component={BlogDetail} />
+          <Route path="/blog" component={Blog}><Blog /></Route>
+          <Route path="/">Home</Route>
         </Switch>
       </div>
     </BrowserRouter>
