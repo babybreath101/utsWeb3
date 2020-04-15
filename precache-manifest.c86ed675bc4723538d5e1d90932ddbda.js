1b738a385f78b9c95d8febd8a0025acbf9741921
babybreath101 <annastasyafar04@gmail.com>
2020-04-15 16:47:02 +0700

    Updates

---
+++ precache-manifest.c86ed675bc4723538d5e1d90932ddbda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe0db4e18dc5c0e91990e249fe635033",
+    "url": "/utsWeb3/index.html"
+  },
+  {
+    "revision": "bfead1f318146b3583c9",
+    "url": "/utsWeb3/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "668bef66efca80737caf",
+    "url": "/utsWeb3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bfead1f318146b3583c9",
+    "url": "/utsWeb3/static/js/2.1622f9c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/utsWeb3/static/js/2.1622f9c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "668bef66efca80737caf",
+    "url": "/utsWeb3/static/js/main.10a69ffa.chunk.js"
+  },
+  {
+    "revision": "5e91958fed93e23acee7",
+    "url": "/utsWeb3/static/js/runtime-main.f9c2851d.js"
+  }
+]);
